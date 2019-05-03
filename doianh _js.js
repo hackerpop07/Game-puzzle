f92f8ce482a2034876cc141167112989eacabbb5
hackerpop07 <hackerpop07@gmail.com>
2019-05-03 09:32:49 +0700

    game-puzzle

---
+++ doianh _js.js
@@ -0,0 +1,110 @@
+let link1 = "1";
+
+function h1() {
+  let anh1 = document.getElementById("h1");
+  if (link1 === "1") {
+    link1 = "6";
+  } else if (link1 === "6") {
+    link1 = "11";
+  } else {
+    link1 = "1";
+  }
+  anh1.src = "image/" +link1 + ".jpg";
+
+  if (link1 === "6" && link2 === '7' && link3 === '8' && link4 === '9' && link5 === '10') {
+    alert("Khỉ");
+  } else if (link1 === "11" && link2 === '12' && link3 === '13' && link4 === '14' && link5 === '15') {
+    alert("Gấu");
+  } else if (link1 === "1" && link2 === '2' && link3 === '3' && link4 === '4' && link5 === '5') {
+    alert("Mèo");
+  }
+}
+
+let link2 = "2";
+
+function h2() {
+  let anh2 = document.getElementById("h2");
+  if (link2 === "2") {
+    link2 = "7";
+  } else if (link2 === "7") {
+    link2 = "12";
+  } else {
+    link2 = "2";
+  }
+  anh2.src ="image/" + link2 + ".jpg";
+
+  if (link1 === "6" && link2 === '7' && link3 === '8' && link4 === '9' && link5 === '10') {
+    alert("Khỉ");
+  } else if (link1 === "11" && link2 === '12' && link3 === '13' && link4 === '14' && link5 === '15') {
+    alert("Gấu");
+  } else if (link1 === "1" && link2 === '2' && link3 === '3' && link4 === '4' && link5 === '5') {
+    alert("Mèo");
+  }
+}
+
+let link3 = "3";
+
+function h3() {
+  let anh3 = document.getElementById("h3");
+  if (link3 === "3") {
+    link3 = "8";
+  } else if (link3 === "8") {
+    link3 = "13";
+  } else {
+    link3 = "3";
+  }
+  anh3.src ="image/" + link3 + ".jpg";
+
+  if (link1 === "6" && link2 === '7' && link3 === '8' && link4 === '9' && link5 === '10') {
+    alert("Khỉ");
+  } else if (link1 === "11" && link2 === '12' && link3 === '13' && link4 === '14' && link5 === '15') {
+    alert("Gấu");
+  } else if (link1 === "1" && link2 === '2' && link3 === '3' && link4 === '4' && link5 === '5') {
+    alert("Mèo");
+  }
+}
+
+let link4 = "4";
+
+function h4() {
+  let anh4 = document.getElementById("h4");
+  if (link4 === "4") {
+    link4 = "9";
+  } else if (link4 === "9") {
+    link4 = "14";
+  } else {
+    link4 = "4";
+  }
+  anh4.src ="image/" + link4 + ".jpg";
+
+  if (link1 === "6" && link2 === '7' && link3 === '8' && link4 === '9' && link5 === '10') {
+    alert("Khỉ");
+  } else if (link1 === "11" && link2 === '12' && link3 === '13' && link4 === '14' && link5 === '15') {
+    alert("Gấu");
+  } else if (link1 === "1" && link2 === '2' && link3 === '3' && link4 === '4' && link5 === '5') {
+    alert("Mèo");
+  }
+}
+
+let link5 = "5";
+
+function h5() {
+  let anh5 = document.getElementById("h5");
+  if (link5 === "5") {
+    link5 = "10";
+  } else if (link5 === "10") {
+    link5 = "15";
+  } else {
+    link5 = "5";
+  }
+  anh5.src = "image/" + link5 + ".jpg";
+  if (link1 === "6" && link2 === '7' && link3 === '8' && link4 === '9' && link5 === '10') {
+    alert("Khỉ");
+  } else if (link1 === "11" && link2 === '12' && link3 === '13' && link4 === '14' && link5 === '15') {
+    alert("Gấu");
+  } else if (link1 === "1" && link2 === '2' && link3 === '3' && link4 === '4' && link5 === '5') {
+    alert("Mèo");
+  }
+}
+
+
